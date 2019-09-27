771caf937cda24160cb77570d3fdff57364c556a
Chris <zhouyao@chargerlink.com>
2019-09-27 11:53:00 +0800

    - fix: 安卓定位问题

--- base/android/src/main/kotlin/me/yohom/amapbase/AMapBasePlugin.kt
+++ base/android/src/main/kotlin/me/yohom/amapbase/AMapBasePlugin.kt
@@ -48,9 +48,7 @@ class AMapBasePlugin {
                                         registrar.activity(),
                                         arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
                                                 Manifest.permission.ACCESS_FINE_LOCATION,
-                                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                                                Manifest.permission.READ_EXTERNAL_STORAGE,
-                                                Manifest.permission.READ_PHONE_STATE),
+                                                Manifest.permission.READ_EXTERNAL_STORAGE),
                                         permissionRequestCode
                                 )
                                 registrar.addRequestPermissionsResultListener { code, _, grantResults ->
